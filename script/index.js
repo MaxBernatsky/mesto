6edab6675c52857fc60b452787eeee623d622a40
Maxim Bernatsky <maxsateon@yandex.ru>
2022-09-18 15:39:31 +0300

    fix: hotfix script.js

--- script/index.js
+++ script/index.js
@@ -3,25 +3,33 @@ let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
 let popup = document.querySelector('.popup');
-let closeBtn = popup.querySelector('.popup__close');
-let inputName = popup.querySelector('.popup__name');
-let inputProfession = popup.querySelector('.popup__profession');
-let popupBtn = popup.querySelector('.popup__button');
+let popupForm = document.querySelector('.popup__form');
+let closeBtn = document.querySelector('.popup__close');
+let inputName = document.querySelector('.popup__name');
+let inputProfession = document.querySelector('.popup__profession');
 
-function popupToggle() {
-  popup.classList.toggle('popup_opened');
+function popupOpen() {
+  popup.classList.add('popup_opened');
+  if (popup.classList.contains('popup_opened')) {
+    inputName.value = profileTitle.textContent;
+    inputProfession.value = profileSubtitle.textContent;
+  }
 }
-editBtn.addEventListener('click', popupToggle);
 
-closeBtn.addEventListener('click', popupToggle);
-
-popupBtn.addEventListener('click', popupToggle);
+function popupClose() {
+  popup.classList.remove('popup_opened');
+}
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-
   profileTitle.textContent = inputName.value;
   profileSubtitle.textContent = inputProfession.value;
+
+  popupClose();
 }
 
-popup.addEventListener('submit', formSubmitHandler);
+popupForm.addEventListener('submit', formSubmitHandler);
+
+editBtn.addEventListener('click', popupOpen);
+
+closeBtn.addEventListener('click', popupClose);
