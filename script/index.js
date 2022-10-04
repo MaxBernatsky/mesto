1027fba5353a560c134715743ec2692d0d081451
Maxim Bernatsky <maxsateon@yandex.ru>
2022-10-04 13:42:12 +0300

    feat: refactoring section place

--- script/index.js
+++ script/index.js
@@ -33,3 +33,50 @@ popupForm.addEventListener('submit', formSubmitHandler);
 editBtn.addEventListener('click', popupOpen);
 
 closeBtn.addEventListener('click', popupClose);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
+
+const placeTemplate = document.querySelector('#place-template').content;
+const container = document.querySelector('.places');
+
+const render = () => {
+  initialCards.forEach((item) => {
+    const currentElement = createItemNode(item.name, item.link);
+    container.append(currentElement);
+  });
+};
+
+const createItemNode = (name, link) => {
+  const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
+  placeElement.querySelector('.place__title').textContent = name;
+  placeElement.querySelector('.place__img').src = link;
+
+  return placeElement;
+};
+
+render();
