5b7dc196e153932eb0f93aadf5a51cb8441f6187
Maxim Bernatsky <maxsateon@yandex.ru>
2022-10-09 12:29:47 +0300

    hotfix: rework popup structure

--- script/index.js
+++ script/index.js
@@ -4,25 +4,31 @@ const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 
 // Popup Elements
-const popup = document.querySelector('.popup');
+const popup = document.querySelector('#popup-profile');
 const popupForm = document.querySelector('.popup__form');
 const closeBtn = document.querySelector('.popup__close');
-const inputName = document.querySelector('.popup__name');
-const inputProfession = document.querySelector('.popup__profession');
+const inputName = document.querySelector('.popup__input_item_name');
+const inputProfession = document.querySelector('.popup__input_item_descr');
 
 // Popup-place Elements
 const popupAddBtn = document.querySelector('.profile__btn-add');
-const popupPlace = document.querySelector('.popup-place');
-const popupPlaceForm = popupPlace.querySelector('.popup-place__form');
-const popupPlaceCloseBtn = popupPlace.querySelector('.popup-place__close');
-const popupPlaceInputName = popupPlace.querySelector('.popup-place__name');
-const popupPlaceInputLink = popupPlace.querySelector('.popup-place__link');
-const popupPlaceBtnCreate = popupPlace.querySelector('.popup-place__button');
+const popupPlace = document.querySelector('#popup-place');
+const popupPlaceForm = popupPlace.querySelector('.popup__form');
+const popupPlaceCloseBtn = popupPlace.querySelector('.popup__close');
+const popupPlaceInputName = popupPlace.querySelector('.popup__input_item_name');
+const popupPlaceInputLink = popupPlace.querySelector(
+  '.popup__input_item_descr'
+);
+const popupPlaceBtnCreate = popupPlace.querySelector('.popup__button');
 
-const popupImg = document.querySelector('.popup-img');
-const popupImgCloseBtn = popupImg.querySelector('.popup-img__close');
-const popupImgFull = popupImg.querySelector('.popup-img__full');
-const popupImgDescr = popupImg.querySelector('.popup-img__descr');
+//Popup-img
+const popupImg = document.querySelector('#popup-img');
+const popupImgCloseBtn = popupImg.querySelector('.popup__close');
+const popupImgFull = popupImg.querySelector('.popup__img');
+const popupImgDescr = popupImg.querySelector('.popup__descr');
+
+const placeTemplate = document.querySelector('#place-template').content;
+const container = document.querySelector('.places');
 
 const popupOpen = () => {
   popup.classList.add('popup_opened');
@@ -33,11 +39,11 @@ const popupOpen = () => {
 };
 
 const popupPlaceOpen = () => {
-  popupPlace.classList.add('popup-place_opened');
+  popupPlace.classList.add('popup_opened');
 };
 
 const popupPlaceClose = () => {
-  popupPlace.classList.remove('popup-place_opened');
+  popupPlace.classList.remove('popup_opened');
 };
 
 const popupClose = () => {
@@ -57,56 +63,20 @@ const placeFormSubmitHandler = (evt) => {
   popupPlaceClose();
 };
 
-popupForm.addEventListener('submit', formSubmitHandler);
-
-popupPlaceForm.addEventListener('submit', placeFormSubmitHandler);
-
-editBtn.addEventListener('click', popupOpen);
-
-closeBtn.addEventListener('click', popupClose);
-
-popupPlaceCloseBtn.addEventListener('click', popupPlaceClose);
-
-popupAddBtn.addEventListener('click', popupPlaceOpen);
-
-popupPlaceBtnCreate.addEventListener('click', popupPlaceClose);
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-  },
-];
-
-const placeTemplate = document.querySelector('#place-template').content;
-const container = document.querySelector('.places');
-
 const render = () => {
   initialCards.forEach((item) => {
     const currentElement = createItemNode(item.name, item.link);
     container.append(currentElement);
   });
   popupPlaceBtnCreate.addEventListener('click', addNewPlace);
+};
+
+const addNewPlace = () => {
+  const newPlace = createItemNode(
+    popupPlaceInputName.value,
+    popupPlaceInputLink.value
+  );
+  container.prepend(newPlace);
 };
 
 const createItemNode = (name, link) => {
@@ -124,8 +94,8 @@ const createItemNode = (name, link) => {
   likeBtn.addEventListener('click', likePlace);
 
   placeImg.addEventListener('click', () => {
-    popupImg.classList.add('popup-img_opened');
-    if (popupImg.classList.contains('popup-img_opened')) {
+    popupImg.classList.add('popup_opened');
+    if (popupImg.classList.contains('popup_opened')) {
       popupImgFull.src = link;
       popupImgDescr.textContent = name;
     }
@@ -134,14 +104,6 @@ const createItemNode = (name, link) => {
   popupImgCloseBtn.addEventListener('click', closeImgPopup);
 
   return placeElement;
-};
-
-const addNewPlace = () => {
-  const newPlace = createItemNode(
-    popupPlaceInputName.value,
-    popupPlaceInputLink.value
-  );
-  container.prepend(newPlace);
 };
 
 const likePlace = (evt) => {
@@ -155,7 +117,21 @@ const deletePlace = (evt) => {
 };
 
 const closeImgPopup = () => {
-  popupImg.classList.remove('popup-img_opened');
+  popupImg.classList.remove('popup_opened');
 };
 
 render();
+
+popupForm.addEventListener('submit', formSubmitHandler);
+
+popupPlaceForm.addEventListener('submit', placeFormSubmitHandler);
+
+editBtn.addEventListener('click', popupOpen);
+
+closeBtn.addEventListener('click', popupClose);
+
+popupPlaceCloseBtn.addEventListener('click', popupPlaceClose);
+
+popupAddBtn.addEventListener('click', popupPlaceOpen);
+
+popupPlaceBtnCreate.addEventListener('click', popupPlaceClose);
